502ac0000378981c171e5adb5af0c82625b5b8b7
racc <racc@github.com>
2011-09-18 10:05:04 +1000

    Finished 51

---
+++ 51/same_digit_primes.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/ruby
+
+def sieve(x)
+	primes = (0..x).to_a
+	limit = (x ** (0.5)).to_i
+
+	(2..limit).each do |s|
+		(2 * s).step(x, s).each { |t| primes[t] = nil }
+	end
+	primes[0..1] = nil
+	primes.compact!
+end
+
+def bit_locations(selector, on = true)
+	locs = []
+	pos = 0
+	selector.each do |d|
+		if (on && d == '1')
+			locs << pos
+		elsif (!on && d == '0')
+			locs << pos
+		end
+		pos += 1
+	end
+	locs
+end
+
+def ons_equal?(p, selector)
+	locs = bit_locations(selector)
+	map = locs.map { |x| p[x] }                                                
+	map.all? { |x| x == p[locs[0]] }
+end
+
+def off_values(p, selector)
+	bit_locations(selector, false).map { |x| p[x] }
+end
+		
+limit = 1000000
+primes = sieve(limit).map { |x| x.to_s.chars.to_a }.group_by { |x| x.size }
+
+primes.each do |size, subset|
+	series = (1..(2**size - 1)).map do |n| 
+		selector = ("%0#{size}b" % n).chars
+		subset.select { |p| ons_equal?(p, selector) }.group_by { |p| off_values(p, selector) }.select {|k, v| v.size == 8 }.values.flatten(1).map { |v| v.join.to_i }
+	end.reject { |s| s.empty? }.flatten
+	
+	p series.first if (!series.empty?)
+end
